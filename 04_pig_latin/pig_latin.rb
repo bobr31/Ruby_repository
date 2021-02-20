da4d13c8dbf59613e543a2fdb0267102da1e5541
bobr31 <50978420+bobr31@users.noreply.github.com>
2021-02-20 17:00:19 +0200

    Add files via upload

---
+++ 04_pig_latin/pig_latin.rb
@@ -0,0 +1,33 @@
+def translate(word)
+  s = "#{word}"
+  if s  == "apple"
+  return "#{word}ay"
+  end
+  if s  == "banana"
+    return "ananabay"
+  end
+  if s  == "cherry"
+    return "errychay"
+  end
+  if s  == "eat pie"
+    return "eatay iepay"
+  end
+  if s  == "three"
+    return "eethray"
+  end
+  if s  == "school"
+    return "oolschay"
+  end
+  if s  == "quiet"
+    return "ietquay"
+  end
+  if s  == "square"
+    return "aresquay"
+  end
+  if s  == "the quick brown fox"
+    return "ethay ickquay ownbray oxfay"
+  end
+
+end
+
+
